7e37345cf45958581134eaf89319c4ae2c073d6f
delfiz-30 <delfiagustina30@gmail.com>
2025-01-11 14:56:16 -0300

    modificacion de ejercicio3

--- ADA_JAVASCRIPT/clase_9/ejercicio3.js
+++ ADA_JAVASCRIPT/clase_9/ejercicio3.js
@@ -4,6 +4,6 @@ let condition1 = prompt("Ingresar true o false para la primera condicion:").toLo
 let condition2 = prompt("Ingresar true o false para la segunda condicion:").toLowerCase() === "true";
 
 console.log(`Condition1 AND Condition2: ${condition1 && condition2}`);
-console.log(`NOT Condition1: ${!condition1}`),
+console.log(`NOT Condition1: ${!condition1}`);
 console.log(`Condition1 OR Condition2: ${condition1 || condition2}`);
 
